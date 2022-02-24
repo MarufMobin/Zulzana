1ceb9c0e3c044ba9cc377b6b8571accdee3b76f1
Maruf Mobin <mobinh264@gmail.com>
2022-02-24 23:43:04 +0600

    [ initial Project Setup ]

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='greeting'>Hellow Maruf Mobin</h1>
+        </div>
+    );
+};
+
+export default Home;
